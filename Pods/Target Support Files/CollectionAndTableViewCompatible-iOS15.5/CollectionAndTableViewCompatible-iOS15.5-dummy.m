8ec7186f6408e718075c47c99da61d3c1a0dc994
Avdeev Ilya <avdeev.ilya.01@gmail.com>
2022-06-17 12:40:31 +0300

    add unit tests

---
+++ Pods/Target Support Files/CollectionAndTableViewCompatible-iOS15.5/CollectionAndTableViewCompatible-iOS15.5-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CollectionAndTableViewCompatible_iOS15_5 : NSObject
+@end
+@implementation PodsDummy_CollectionAndTableViewCompatible_iOS15_5
+@end
